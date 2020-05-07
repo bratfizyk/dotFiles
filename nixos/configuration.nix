1635529c6f32783d2df3ff70558d489f669734ac
Alojzy Leszcz <alojzy.leszcz@gmail.com>
2020-05-07 23:52:04 +0200

    Adding more initial config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./cachix.nix
     ];
 
   # Use the GRUB 2 boot loader.
@@ -30,9 +31,12 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
+  console = {
+    keyMap = "pl";
+    font = "Lat2-Terminus16";
+  };
+
   i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "pl";
     defaultLocale = "en_US.UTF-8";
   };
 
@@ -43,41 +47,32 @@
   # $ nix search wget
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-    wget vim zsh vscode firefox git oh-my-zsh 
-    # ghc cabal-install stack cabal2nix
-    # partition-manager
-    # ghcide
-    # (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
+    curl vim zsh vscode firefox git oh-my-zsh qutebrowser
+    gcc ghc cabal-install
+    rxvt_unicode
+    clipmenu
+    (import (builtins.fetchTarball "https://cachix.org/api/v1/install") {}).cachix
+    (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
   ];
 
   # (oh-my-)zsh
-  programs.zsh.enable = true;
-  programs.zsh.autosuggestions.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.ohMyZsh = {
+  programs.zsh = {
     enable = true;
-    plugins = [ "git" "sudo" ];
-    theme = "agnoster";
+    autosuggestions.enable = true;
+    enableCompletion = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "sudo" ];
+      theme = "agnoster";
+    };
   };
+  environment.variables.EDITOR = "urxvt";
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  fonts.fonts = with pkgs; [
+    hermit
+    source-code-pro
+    terminus_font
+  ];
 
   # Enable sound.
   sound.enable = true;
@@ -90,25 +85,23 @@
     layout = "us";
     
     # Enable the KDE Desktop Environment.
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
+    #displayManager.sddm.enable = true;
+    #desktopManager.plasma5.enable = true;
+    
+    displayManager = {
+      lightdm.enable = true;
+      defaultSession =  "none+i3";
+    };
 
-    #desktopManager = {
-    #  default = "xfce";
-    #  xterm.enable = false;
-    #  xfce = {
-    #    enable = true;
-    #    noDesktop = true;
-    #    enableXfwm = false;
-    #  };
-    #};
-    #windowManager.i3 = {
-    #  enable = true;
-    #  package = pkgs.i3-gaps;
-    #  extraPackages = with pkgs; [
-    #    dmenu i3status i3lock i3blocks
-    #  ];
-    #};
+    desktopManager.xterm.enable = false;
+    
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        dmenu i3status i3lock i3blocks
+      ];
+    };
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -118,16 +111,12 @@
     extraGroups = [ "wheel" "vboxusers" ];
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "20.03";
 
   # Make VirtualBox happy
   security.rngd.enable = false;
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.x11 = true;
-  # virtualisation.virtualbox.host.enable = true;
-  # virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 }
