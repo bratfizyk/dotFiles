b64f6536fd6e4f572218ee32b7ab3268d1183aa6
Alojzy Leszcz <>
2020-05-11 02:52:03 +0200

    Reenabling ghcide

--- nixos/home/haskell.nix
+++ nixos/home/haskell.nix
@@ -2,7 +2,8 @@
 
 {
     home.packages = with pkgs; [
-        #(import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
+        cachix
+        (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
         ghc 
         cabal-install
     ];
