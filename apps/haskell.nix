a6feb741be2cac9875c8e5c7d8f0ef97b2418a63
Alojzy Leszcz <aleszcz@pm.me>
2023-11-19 23:56:54 +0100

    Adding Haskell

---
+++ apps/haskell.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    gcc-unwrapped
+    ghc 
+    cabal-install
+    cabal2nix
+    stack
+    haskellPackages.ghcide
+    haskellPackages.haskell-language-server
+  ];
+}
