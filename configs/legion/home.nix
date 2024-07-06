a4e34e935afe40b67883383bf9cd01d8c25a156b
Alojzy Leszcz <aleszcz@pm.me>
2024-07-06 16:43:23 +0200

    Small change to home

--- configs/legion/home.nix
+++ configs/legion/home.nix
@@ -36,7 +36,6 @@
     homeDirectory = "/home/beko";
     stateVersion = "24.05";
 
-
     packages = with pkgs; [
       discord
       efibootmgr
@@ -59,21 +58,8 @@
       zoom-us
     ];
 
-    file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
-    };
-
     sessionVariables = {
-      EDITOR = "vim";
+      EDITOR = "nvim";
       TERMINAL = "alacritty";
       BROWSER = "firefox";
       FLAKE = "/home/beko/Projects/dotFiles";
