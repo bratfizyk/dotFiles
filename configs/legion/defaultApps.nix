d82d3b3edfe1553ebfa217b58c31aa43d7d1e3f5
Alojzy Leszcz <aleszcz@pm.me>
2024-07-13 18:40:33 +0200

    Adding firefox to mime default apps

--- configs/legion/defaultApps.nix
+++ configs/legion/defaultApps.nix
@@ -28,6 +28,16 @@
         "image/png" = [ "loupe.desktop" ];
         "image/jpeg" = [ "loupe.desktop" ];
         "application/pdf" = [ "evince.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+        "text/html" = [ "firefox.desktop" ];
+        "application/x-extension-htm" = [ "firefox.desktop" ];
+        "application/x-extension-html" = [ "firefox.desktop" ];
+        "application/x-extension-shtml" = [ "firefox.desktop" ];
+        "application/xhtml+xml" = [ "firefox.desktop" ];
+        "application/x-extension-xhtml" = [ "firefox.desktop" ];
+        "application/x-extension-xht" = [ "firefox.desktop" ];
       };
     };
   };
