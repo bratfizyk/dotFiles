77925e4ab698aa9dd3292d52cfe34a690f37bb81
Alojzy Leszcz <aleszcz@pm.me>
2024-10-03 19:21:26 +0200

    Removing an old command from hyprland config

--- apps/hyprland/settings.nix
+++ apps/hyprland/settings.nix
@@ -38,11 +38,11 @@
     "$mod, F7, exec, busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 1500"
     "$mod, F8, exec, busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500"
     
+    "$mod, C, exec, chromium"
     "$mod, D, exec, ags -t applauncher"
     "$mod, E, exec, nemo"
     "$mod, F, exec, firefox"
     "$mod, G, exec, gnucash"
-    "$mod, I, exec, swaync-client -t -sw"
     "$mod, K, exec, keepassxc"
     "$mod, L, exec, swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --fade-in 0.2"
     "$mod, Q, exec, qalculate-gtk"
