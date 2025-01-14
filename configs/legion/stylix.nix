17fcce35481cec44b67e7841a6bd7d0107b58be2
Alojzy Leszcz <aleszcz@pm.me>
2025-01-14 21:39:43 +0100

    Updating flake. Changing colorscheme to Tokyo Night

--- configs/legion/stylix.nix
+++ configs/legion/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     image = ../../assets/wallpapers/Forest-Autumn.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-city-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     fonts = {
       sansSerif = {
         package = pkgs.noto-fonts;
