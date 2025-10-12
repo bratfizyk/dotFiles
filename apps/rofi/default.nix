779e90a5d2348059cfc431bf81b847f7a20e3321
Alojzy Leszcz <alojzy.leszcz.semester130@passinbox.com>
2025-10-12 22:05:01 +0200

    Updating system

--- apps/rofi/default.nix
+++ apps/rofi/default.nix
@@ -3,7 +3,7 @@
 {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland;
+    package = pkgs.rofi;
     theme = (import ./theme.nix { inherit pkgs; inherit config; }).theme;
   };
 }
