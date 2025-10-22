6f9f881c16005d6e84bf4e0a5b797c8ef0e1017e
Alojzy Leszcz <alojzy.leszcz.semester130@passinbox.com>
2025-10-22 23:30:08 +0200

    Returning to GNOME

--- configs/legion/configuration.nix
+++ configs/legion/configuration.nix
@@ -9,10 +9,10 @@
       ./stylix.nix
 
       ../../modules/audio.nix
-      ../../modules/cinnamon.nix
+      #../../modules/cinnamon.nix
       #../../modules/cosmic.nix
       ../../modules/ecryptfs.nix
-      #../../modules/gnome.nix
+      ../../modules/gnome.nix
       ../../modules/hyprland.nix
       #../../modules/kde.nix
       ../../modules/locale.nix
